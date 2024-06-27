2b6491eeeb9d9744ade789348a3f7b1bf7da5c5b
Neeraj Kumar <nk061242@gmail.com>
2024-06-27 20:15:05 +0530

    Lists and array in python

--- Basics/List_OR_Array.py
+++ Basics/List_OR_Array.py
@@ -1,4 +1,9 @@
 teas = ['Black','Lemon','Green','Oolong'];
 # print(teas[1:4])
-for tea in teas:
-    print(tea)
+# for tea in teas:
+#     print(tea)
+
+
+# for(int i=;i<10;i++){
+#     print(i)
+# }
